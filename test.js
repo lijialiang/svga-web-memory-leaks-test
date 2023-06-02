451d80217207bb99cff15d1c28a33f2d8fd1f628
lijialiang <lijialiang@joyy.com>
2023-06-02 10:08:21 +0800

    feat(memlab): action

--- test.js
+++ test.js
@@ -2,4 +2,8 @@ function url() {
   return 'http://127.0.0.1:8080/'
 }
 
-module.exports = { url }
+async function action(page) {
+  await new Promise(resolve => setTimeout(resolve, 10000))
+}
+
+module.exports = { action, url }
